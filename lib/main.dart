3063d2103c7c390a54edb23def9f0ef2767a4bbf
--global <johndoe@example.com>
2020-06-15 17:39:23 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Map data;
+  List userData;
+
+  Future getData() async {
+    http.Response response = await http.get("https://reqres.in/api/users?page=2"
+//        "https://api.unsplash.com/photos/?client_id=cf49c08b444ff4cb9e4d126b7e9f7513ba1ee58de7906e4360afc1a33d1bf4c0"
+        );
+    data = json.decode(response.body);
+
+    setState(() {
+      userData = data["data"];
+    });
+    debugPrint(data.toString());
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Fake Friends"),
+        backgroundColor: Colors.green,
+      ),
+      body: Container(),
+    );
+  }
+}
